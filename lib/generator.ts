df133689a6ae1875647e15342587aec56e0c28fa
Enngage <ennspassion@gmail.com>
2021-10-08 12:11:33 +0200

    feat: implements timestamp disable option, adds colors for better console output, adds prettier and beautifies output with the ability to pass custom configuration

--- lib/generator.ts
+++ lib/generator.ts
@@ -1,8 +1,9 @@
 import { IContentType, DeliveryClient } from '@kentico/kontent-delivery';
 import * as fs from 'fs';
-import { name } from '../package.json';
 
 import { modelHelper } from './model-helper';
+import { Options } from 'prettier';
+import { green, red, yellow } from 'colors';
 
 export class Generator {
     private readonly deliveryClient: DeliveryClient;
@@ -26,35 +27,39 @@ export class Generator {
         });
     }
 
-    async generateModelsAsync(): Promise<void> {
-        console.log(`${name} started`);
+    async generateModelsAsync(config?: { formatOptions: Options }): Promise<void> {
+        console.log(green(`Model generator started \n`));
 
         try {
             const types = (await this.deliveryClient.types().toAllPromise()).data.items;
 
+            console.log(`Found '${types.length}' content types: \n`);
+
             for (const type of types) {
-                this.generateClass(type);
+                this.generateClass({
+                    type: type,
+                    formatOptions: config?.formatOptions
+                });
+                console.log(`${yellow(modelHelper.getFilename({ type: type }))} (${type.system.name})`);
             }
+
+            console.log(green(`\nModel generator completed`));
         } catch (error) {
-            console.log(`${name} failed with error:`);
+            console.log(red(`Failed with error:`));
             console.log(error);
             throw error;
         }
     }
 
-    private generateClass(type: IContentType): void {
-        const classFileName = modelHelper.getFilename({ type: type });
-        const classContent = modelHelper.getClassDefinition({
-            type: type,
-            addTimestamp: this.addTimestamp
+    private generateClass(config: { formatOptions?: Options; type: IContentType }): void {
+        const classFileName = modelHelper.getFilename({ type: config.type });
+        const code = modelHelper.getClassDefinition({
+            type: config.type,
+            addTimestamp: this.addTimestamp,
+            formatOptions: config.formatOptions
         });
 
-        // create class file
-        fs.writeFile('./' + classFileName, classContent, (error) => {
-            if (error) {
-                throw Error(`Could not create class file '${classFileName}'`);
-            }
-            console.log(`Class '${classFileName}' was created`);
-        });
+        // create file
+        fs.writeFileSync('./' + classFileName, code);
     }
 }
