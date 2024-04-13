c2e12658c2fbdfca0d82a98bb0156f1121fc4095
Richard Sustek <ennspassion@gmail.com>
2024-04-13 10:49:19 +0200

    feat: Handle errors in file formatting with prettier, display better info message and store unformatted file

--- lib/file-helper.ts
+++ lib/file-helper.ts
@@ -6,10 +6,19 @@ import * as fs from 'fs';
 export class FileHelper {
     async createFileOnFsAsync(text: string, filename: string, formatOptions: Options | undefined): Promise<void> {
         const finalFilename = `${filename}`;
-        const contentToStore = await formatHelper.formatCodeAsync(text, formatOptions);
+        try {
+            const contentToStore = await formatHelper.formatCodeAsync(text, formatOptions);
 
-        fs.writeFileSync('./' + finalFilename, contentToStore);
-        console.log(`Created '${Colors.yellow(finalFilename)}'`);
+            fs.writeFileSync('./' + finalFilename, contentToStore);
+            console.log(`Created '${Colors.yellow(finalFilename)}'`);
+        } catch (error) {
+            console.log(`Failed to format file '${Colors.red(filename)}'. Skipping prettier for this file.`);
+
+            const contentToStore = text;
+
+            fs.writeFileSync('./' + finalFilename, contentToStore);
+            console.log(`Created '${Colors.yellow(finalFilename)}'`);
+        }
     }
 
     createDir(dirPath: string): void {
