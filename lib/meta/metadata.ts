78e1d4e0f68209c8ff901c792533a3b1bf9d8f6c
Richard Sustek <ennspassion@gmail.com>
2024-06-28 13:25:22 +0200

    release

--- lib/meta/metadata.ts
+++ lib/meta/metadata.ts
@@ -1,6 +1,6 @@
 
 export const libMetadata = {
 	name: '@kontent-ai/model-generator',
-    timestamp: 'Tue, 18 Jun 2024 06:30:41 GMT',
-    version: '7.2.0'
+    timestamp: 'Fri, 28 Jun 2024 11:25:16 GMT',
+    version: '7.3.0'
 };
