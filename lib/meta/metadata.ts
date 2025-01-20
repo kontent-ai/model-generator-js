c32429e8b100dea046c49d1c28e1f64b6d37e0b1
“Daniel <danielomalley@redcross.org.uk>
2025-01-20 10:57:59 +0000

    chore: update version numbers

--- lib/meta/metadata.ts
+++ lib/meta/metadata.ts
@@ -1,6 +1,6 @@
 
 export const libMetadata = {
 	name: '@kontent-ai/model-generator',
-    timestamp: 'Thu, 15 Aug 2024 09:05:18 GMT',
-    version: '7.4.0'
+    timestamp: 'Mon, 20 Jan 2025 10:52:00 GMT',
+    version: '7.4.1'
 };
