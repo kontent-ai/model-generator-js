926c4a7171461c367ee860f7a68d9db70631f87f
Richard Sustek <ennspassion@gmail.com>
2025-02-11 09:25:43 +0100

    improves tsconfig setup, only adds lib to dist folder

--- scripts/update-version.ts
+++ scripts/update-version.ts
@@ -1,10 +1,10 @@
-import { name, version } from '../package.json' with { type: 'json' };
+import packageJson from '../package.json' with { type: 'json' };
 import { createVersionFile } from './utils/script.utils.js';
 
 createVersionFile({
     date: new Date(),
     filePath: './lib/meta/metadata.ts',
     propertyName: 'libMetadata',
-    packageName: name,
-    packageVersion: version
+    packageName: packageJson.name,
+    packageVersion: packageJson.version
 });
