3a7b12ede6a639d7cdacc868e9ff32425cbe0e78
Richard Sustek <ennspassion@gmail.com>
2025-02-14 09:15:37 +0100

    Update metadata.ts

--- lib/meta/metadata.ts
+++ lib/meta/metadata.ts
@@ -2,6 +2,6 @@
 export const libMetadata = {
     host: 'npmjs.com',
 	name: '@kontent-ai/model-generator',
-    timestamp: 'Thu, 13 Feb 2025 10:56:41 GMT',
-    version: '8.0.0-15'
+    timestamp: 'Fri, 14 Feb 2025 08:15:23 GMT',
+    version: '8.0.0-16'
 };
