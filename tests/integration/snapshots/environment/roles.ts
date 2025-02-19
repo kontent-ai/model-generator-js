69960ceb24d0e7c19baaae3a6d09c7f6949fe8a5
Richard Sustek <ennspassion@gmail.com>
2025-02-19 12:00:44 +0100

    update snapshots

--- tests/integration/snapshots/environment/roles.ts
+++ tests/integration/snapshots/environment/roles.ts
@@ -1,1 +1,37 @@
-export const roles = {} as const;
+export const roles = {
+    /**
+     * Content contributor
+     */
+    contentContributor: {
+        codename: undefined,
+        id: 'dca0ac85-274f-429e-b00e-6f74bf917051',
+        name: 'Content contributor'
+    },
+
+    /**
+     * Reviewer
+     */
+    reviewer: {
+        codename: undefined,
+        id: '94478afa-d898-45dd-8300-ba61b6e0eb4b',
+        name: 'Reviewer'
+    },
+
+    /**
+     * Project manager
+     */
+    projectManager: {
+        codename: 'project-manager',
+        id: 'af95ac37-4efa-4b92-aa26-db773cd172c6',
+        name: 'Project manager'
+    },
+
+    /**
+     * Developer
+     */
+    developer: {
+        codename: undefined,
+        id: 'b3994090-0818-4969-816b-f4c5d3e526f3',
+        name: 'Developer'
+    }
+} as const;
